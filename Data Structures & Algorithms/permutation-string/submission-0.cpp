b0ed7d5fba54a910860824d132253e44651699b8
Sanskar Agrawal <sanskaragrawal0608@gmail.com>
2026-06-12 21:14:40 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n1 =s1.length();
+        int n2 =s2.length();
+        if(n1>n2){
+            return false;
+        }
+        sort(s1.begin(),s1.end());
+       for(int i=0; i<=n2-n1; i++){
+        string substring = s2.substr(i,n1);
+            sort(substring.begin(),substring.end());
+            if(s1==substring){
+                return true;
+            }
+       }
+       return false;
+    }
+};
